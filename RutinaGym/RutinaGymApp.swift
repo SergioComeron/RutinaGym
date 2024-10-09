c556fbb5dcd13282fc3f951084e1f4d9e35ae32a
SergioComeron <sergiocomeron@icloud.com>
2024-10-09 16:55:51 +0200

    Initial Commit

---
+++ RutinaGym/RutinaGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  RutinaGymApp.swift
+//  RutinaGym
+//
+//  Created by Sergio Comerón Sánchez-Pani on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RutinaGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
